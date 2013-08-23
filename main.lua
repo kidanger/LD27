1b1f29e67f4de14e4bd3e0e19c8dbf110ff0263d
kidanger <angerj.dev@gmail.com>
2013-08-23 21:40:27 +0200

    Add basic main.lua

---
+++ main.lua
@@ -0,0 +1,43 @@
+require 'drystal'
+local font = require 'truetype'
+local physic = require 'physic'
+local particle = require 'particle'
+-- not gonna use 'net' extension
+-- 'web', I don't know yet
+local timer = require 'hump/timer'
+
+width, height = 600, 480
+
+
+--[[===================
+----======= INIT ======]]
+function init()
+end
+
+
+--[[=====================
+----======= UPDATE ======]]
+function update(dt)
+end
+
+
+--[[===================
+----======= DRAW ======]]
+function draw()
+end
+
+
+--[[=====================
+----======= EVENTS ======]]
+
+function key_press(key)
+end
+function key_release(key)
+end
+
+function mouse_motion(x, y, dx, dy)
+end
+function mouse_press(x, y, b)
+end
+function mouse_release(x, y, b)
+end
