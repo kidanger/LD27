f32ec7b431b7cede4c7e154a80724bb9f5371466
kidanger <angerj.dev@gmail.com>
2013-08-23 22:02:17 +0200

    Add the basic state thing

--- main.lua
+++ main.lua
@@ -8,22 +8,27 @@ local timer = require 'hump/timer'
 
 width, height = 600, 480
 
+local state = {}
 
 --[[===================
 ----======= INIT ======]]
 function init()
+    resize(width, height)
 end
 
 
 --[[=====================
 ----======= UPDATE ======]]
 function update(dt)
+    state.update(dt)
+    timer.update(dt)
 end
 
 
 --[[===================
 ----======= DRAW ======]]
 function draw()
+    state.draw()
 end
 
 
@@ -31,13 +36,28 @@ end
 ----======= EVENTS ======]]
 
 function key_press(key)
+    if state.key_press then
+        state.key_press(key)
+    end
 end
 function key_release(key)
+    if state.key_release then
+        state.key_release(key)
+    end
 end
 
 function mouse_motion(x, y, dx, dy)
+    if state.key_motion then
+        state.key_motion(x, y, dx, dy)
+    end
 end
 function mouse_press(x, y, b)
+    if state.key_press then
+        state.key_press(x, y, b)
+    end
 end
 function mouse_release(x, y, b)
+    if state.key_release then
+        state.key_motion(x, y, b)
+    end
 end
